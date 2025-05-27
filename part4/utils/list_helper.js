8e13a08e2e33e287f202a23f370fca63e101eee0
Leoncio Aliaga Saldaña <60636702+leoaliaga10@users.noreply.github.com>
2025-05-26 19:03:45 -0500

    Commit exercise 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,9 +1,15 @@
 const dummy = (blogs) => {
     return 1
   }
+
+  const totalLikes = (blogs) => {
+    //blogs es array de objetos
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+  }
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
 
 
